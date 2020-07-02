9d9a3d9f40a9f4b42709c398be80f3861cb220ea
Joao Grilo <joao.grilo@gmail.com>
2020-07-03 00:25:42 +0200

    Add documentation, remove debugging.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Usage
+
+export TRACE=1 # For debugging
+./ado-jenkins-build -u https://jenkins-host.corporate/job/testbuild
--- build.sh
+++ build.sh
@@ -12,8 +12,3 @@ buildTimestamp=$(date --iso-8601=seconds)
 gofmt -w *.go
 
 go build -ldflags "-X 'main.version=$version' -X 'main.commit=$commit' -X 'main.buildTimestamp=$buildTimestamp'"
-
-echo ""
-echo ""
-
-TRACE=1 BUILD_SOURCEBRANCH=hello ./ado-jenkins-build -g -u http://localhost:8080/job/testjob
