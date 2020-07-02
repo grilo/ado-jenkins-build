18b007252a338a004804294e9f7d9c5f0f5be504
Joao Grilo <joao.grilo@gmail.com>
2020-07-03 00:23:22 +0200

    Remove gitlab references.

--- main.go
+++ main.go
@@ -20,7 +20,6 @@ func main() {
 
 	parser := argparse.NewParser("ado-jenkins-build", "Executes a jenkins job from Azure DevOps and waits for its completion. Returns 0 on SUCCESS, 1 on FAILURE, 2 on ABORTED, 3 on UNSTABLE and 4 on NOT_BUILT.")
 
-	argEmulateGitLab := parser.Flag("g", "gitlab", &argparse.Options{Help: "Emulate gitlab variables (useful when migrating from GitLab)."})
 	argTimeout := parser.Int("t", "timeout", &argparse.Options{
 		Help:    "Maximum number of seconds to wait (poll).",
 		Default: 3600, // One hour
@@ -35,10 +34,6 @@ func main() {
 		log.Fatal(parser.Usage(parseErr))
 	}
 
-	if *argEmulateGitLab {
-		log.Printf("GitLab emulation enabled.")
-	}
-
 	parsedUrl, err := url.Parse(*argUrl)
 	if err != nil {
 		log.Fatalf("Unable to parse url: %s", argUrl)
@@ -48,7 +43,7 @@ func main() {
 		log.Fatalf("Unknown protocol: %s (%s)", parsedUrl.Scheme, *argUrl)
 	}
 
-	variables := ReadEnvironment(*argEmulateGitLab)
+	variables := ReadEnvironment()
 
 	response := TriggerBuild(parsedUrl, variables)
 
--- vars.go
+++ vars.go
@@ -6,24 +6,7 @@ import (
 	"strings"
 )
 
-func glabTranslate(variable string) string {
-
-	switch variable {
-
-	case "BUILD_REPOSITORY_URI":
-		newName := "gitlabSourceRepoURL"
-		log.Printf("Translating %s into: %s", variable, newName)
-		return newName
-	case "BUILD_SOURCEBRANCH":
-		newName := "gitlabSourceBranch"
-		log.Printf("Translating %s into: %s", variable, newName)
-		return newName
-	}
-
-	return variable
-}
-
-func ReadEnvironment(emulateGitLab bool) map[string]string {
+func ReadEnvironment() map[string]string {
 
 	/*
 	   See: https://docs.microsoft.com/en-us/azure/devops/pipelines/build/variables?view=azure-devops&tabs=yaml
@@ -33,9 +16,6 @@ func ReadEnvironment(emulateGitLab bool) map[string]string {
 
 	vars := make(map[string]string)
 
-	//BUILD_REPOSITORY_URI
-	//BUILD_SOURCEBRANCHNAME
-
 	for _, e := range os.Environ() {
 		pair := strings.SplitN(e, "=", 2)
 		key := pair[0]
@@ -43,11 +23,7 @@ func ReadEnvironment(emulateGitLab bool) map[string]string {
 
 		if strings.HasPrefix(key, "AGENT_") || strings.HasPrefix(key, "BUILD_") || strings.HasPrefix(key, "PIPELINE_") || strings.HasPrefix(key, "SYSTEM_") {
 			log.Printf("Found %s: %s", key, value)
-			if emulateGitLab {
-				vars[glabTranslate(key)] = value
-			} else {
-				vars[key] = value
-			}
+            vars[key] = value
 		}
 	}
 
